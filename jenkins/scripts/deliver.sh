197e7251e76300be040a36d9f064439ea096cbb0
bahar-taghavi <166640392+bahar-taghavi@users.noreply.github.com>
2024-04-10 18:45:04 -0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,11 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
+
+echo Name:
+echo ${NAME}
+echo Version:
+echo ${VERSION}
+
 chmod 777 ./target/${NAME}-${VERSION}.jar
 java -jar ./target/${NAME}-${VERSION}.jar
